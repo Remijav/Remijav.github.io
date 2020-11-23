d2c642186c05b04575103771b55c802f0c3dd7d1
Rémi Javierre <javremi@gmail.com>
2020-11-23 12:26:50 +0100

    sw

--- sw.js
+++ sw.js
@@ -1,69 +1,69 @@
-    
-    self.addEventListener('install', (e) => {
-        console.log('[Service Worker] Install');
-    });
-    
-    var cacheName = 'ginkobusPWA-v1';
+"use strict";
 
-    var contentToCache = [
-      './index.html',
-      './manifest.webmanifest",
-      './app.js',
-      './style.css',
-      './icons/favicon.ico',
-      './icons/icon-32.png',
-      './icons/icon-64.png',
-      './icons/icon-96.png',
-      './icons/icon-128.png',
-      './icons/icon-168.png',
-      './icons/icon-180.png',
-      './icons/icon-192.png',
-      './icons/icon-256.png',
-      './icons/icon-512.png',
-      './icons/maskable_icon.png'
-    ];
-    
-    
+/** 
+    Source: https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+*/
+
+// variable definitions 
+var CACHE_NAME = 'ginkobusPWA-v1';
+
+var contentToCache = [
+  './index.html',    
+  './manifest.webmanifest',    
+  './style.css', 
+  './app.js', 
+  './icons/favicon.ico',
+  './icons/icon-32.png',
+  './icons/icon-64.png',
+  './icons/icon-96.png',
+  './icons/icon-128.png',
+  './icons/icon-168.png',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-256.png',
+  './icons/icon-512.png',
+  './icons/maskable_icon.png'
+];
+
+// service worker installation
+self.addEventListener('install', function(e) {
+    console.log('[Service Worker] Install');
+    e.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+        console.log('[Service Worker] Caching application content & data');
+        return cache.addAll(contentToCache);
+    }));
+});
 
 
+self.addEventListener('fetch', (e) => {
+   
+    // Stratégie initiale : cache ou network avec mise en cache 
+    e.respondWith(
+        caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+            return r || 
+                fetch(e.request).then((response) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        console.log('[Service Worker] Caching new resource: '+e.request.url);
+                        cache.put(e.request, response.clone());
+                        return response;
+                    });
+                });
+        })
+    );
+});
     
-    self.addEventListener('install', (e) => {
-    	  console.log('[Service Worker] Install');
-    	  e.waitUntil(
-    	    caches.open(cacheName).then((cache) => {
-    	          console.log('[Service Worker] Caching all: app shell and content');
-    	      return cache.addAll(contentToCache);
-    	    })
-    	  );
-    	});
-    
-    self.addEventListener('fetch', (e) => {
-        console.log('[Service Worker] Fetched resource '+e.request.url);
-    });
-    
-    self.addEventListener('fetch', (e) => {
-    	  e.respondWith(
-    	    caches.match(e.request).then((r) => {
-    	          console.log('[Service Worker] Fetching resource: '+e.request.url);
-    	      return r || fetch(e.request).then((response) => {
-    	                return caches.open(cacheName).then((cache) => {
-    	          console.log('[Service Worker] Caching new resource: '+e.request.url);
-    	          cache.put(e.request, response.clone());
-    	          return response;
-    	        });
-    	      });
-    	    })
-    	  );
-    	});
-    
-    self.addEventListener('activate', (e) => {
-    	  e.waitUntil(
-    	    caches.keys().then((keyList) => {
-    	          return Promise.all(keyList.map((key) => {
-    	        if(key !== cacheName) {
-    	          return caches.delete(key);
-    	        }
-    	      }));
-    	    })
-    	  );
-    	});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        // cleaning previous caches
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if(CACHE_NAME.indexOf(key) === -1) {
+                    console.log("[Service Worker] Cleaning old cache");
+                    return caches.delete(key);
+                }
+          }));
+        })
+    );
+});
